019b5ee14ee6d0048dfd5f33a28dbdba6da3da89
ambo <andreas.maengel@gmail.com>
2020-11-03 12:03:45 +0100

    removed issue preview from kurzgefasst and chronik, added term based class to full nodes, added latest assets from blaetter theme

--- bundle/3-6d8eedeeb99251b34a82.js
+++ bundle/3-6d8eedeeb99251b34a82.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[3],{12:function(n,w,o){}}]);
-//# sourceMappingURL=3-d7eafb4c860dc5dea7a5.js-d7eafb4c860dc5dea7a5.map
+//# sourceMappingURL=3-6d8eedeeb99251b34a82.js-6d8eedeeb99251b34a82.map
