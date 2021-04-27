e7de252942e738b8d636fd02393f3608d24d5c26
ambo <andreas.maengel@gmail.com>
2021-04-27 10:21:34 +0200

    added latest assets from blaetter-theme (new swiper js and styles)

--- bundle/3-71d1b0a697d14da4634a.js
+++ bundle/3-71d1b0a697d14da4634a.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[3],{12:function(n,w,o){}}]);
-//# sourceMappingURL=3-6d8eedeeb99251b34a82.js-6d8eedeeb99251b34a82.map
+//# sourceMappingURL=3-71d1b0a697d14da4634a.js-71d1b0a697d14da4634a.map
