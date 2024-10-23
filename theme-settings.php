ac942040c9459bb5ed57350435d0bd48a0b87a62
ambo <andreas.maengel@gmail.com>
2024-10-23 21:47:45 +0200

    added settings for the promo banner, refactored palm settings

--- theme-settings.php
+++ theme-settings.php
@@ -20,48 +20,8 @@ function palm_form_system_theme_settings_alter(
     $form['palm_settings'] = [
         '#type'   => 'details',
         '#title'  => t('advanced theme settings'),
+        '#description' => t('Administrate settings for the palm theme'),
         '#open'   => true,
-    ];
-
-    $form['palm_settings']['palm_footer_link'] = [
-        '#type'           => 'textfield',
-        '#title'          => t('Node ID for footer link'),
-        '#description'    => t('Please provide a Node ID for the link at the bottom of the page around "Karl Barth".'),
-        '#default_value'  => theme_get_setting('palm_footer_link'),
-        '#size'           => 10,
-    ];
-
-    $form['palm_settings']['palm_footer_link_title'] = [
-        '#type'           => 'textfield',
-        '#title'          => t('title for footer link'),
-        '#description'    => t('Please provide a title for the link at the bottom of the page around "Karl Barth".'),
-        '#default_value'  => theme_get_setting('palm_footer_link_title'),
-    ];
-
-    $form['palm_settings']['palm_footer_cancellation_button_link'] = [
-      '#type'           => 'textfield',
-      '#title'          => t('URL for the cancellation button link'),
-      '#description'    => t('Please provide the url of the cancellation form.'),
-      '#default_value'  => theme_get_setting('palm_footer_cancellation_button_link'),
-    ];
-    $form['palm_settings']['palm_footer_cancellation_button_link_title'] = [
-      '#type'           => 'textfield',
-      '#title'          => t('title for cancellation button link'),
-      '#description'    => t('Please provide a title for the cancellation button in the footer.'),
-      '#default_value'  => theme_get_setting('palm_footer_cancellation_button_link_title'),
-    ];
-
-    $form['palm_settings']['palm_header_promo_button_link'] = [
-        '#type'           => 'textfield',
-        '#title'          => t('URL for the promo button link'),
-        '#description'    => t('Please provide the url of the promo link. Leave empty to disable the promo button.'),
-        '#default_value'  => theme_get_setting('palm_header_promo_button_link'),
-    ];
-    $form['palm_settings']['palm_header_promo_button_link_title'] = [
-        '#type'           => 'textfield',
-        '#title'          => t('title for promo button link'),
-        '#description'    => t('Please provide a title for the promo button in the footer.'),
-        '#default_value'  => theme_get_setting('palm_header_promo_button_link_title'),
     ];
 
     $form['palm_settings']['palm_initial_nodetypes'] = [
@@ -73,4 +33,103 @@ function palm_form_system_theme_settings_alter(
         '#default_value'  => theme_get_setting('palm_initial_nodetypes'),
         '#size'           => 10
     ];
+
+    $form['palm_footer_settings'] = [
+      '#type'   => 'details',
+      '#title'  => t('footer settings'),
+      '#description' => t('Administrate settings for the footer.'),
+      '#open'   => true,
+    ];
+
+    $form['palm_footer_settings']['palm_footer_link'] = [
+        '#type'           => 'textfield',
+        '#title'          => t('Node ID for footer link'),
+        '#description'    => t('Please provide a Node ID for the link at the bottom of the page around "Karl Barth".'),
+        '#default_value'  => theme_get_setting('palm_footer_link'),
+        '#size'           => 10,
+    ];
+
+    $form['palm_footer_settings']['palm_footer_link_title'] = [
+        '#type'           => 'textfield',
+        '#title'          => t('title for footer link'),
+        '#description'    => t('Please provide a title for the link at the bottom of the page around "Karl Barth".'),
+        '#default_value'  => theme_get_setting('palm_footer_link_title'),
+    ];
+
+    $form['palm_footer_settings']['palm_footer_cancellation_button_link'] = [
+      '#type'           => 'textfield',
+      '#title'          => t('URL for the cancellation button link'),
+      '#description'    => t('Please provide the url of the cancellation form.'),
+      '#default_value'  => theme_get_setting('palm_footer_cancellation_button_link'),
+    ];
+    $form['palm_footer_settings']['palm_footer_cancellation_button_link_title'] = [
+      '#type'           => 'textfield',
+      '#title'          => t('title for cancellation button link'),
+      '#description'    => t('Please provide a title for the cancellation button in the footer.'),
+      '#default_value'  => theme_get_setting('palm_footer_cancellation_button_link_title'),
+    ];
+
+    $form['palm_promo_settings'] = [
+      '#type'   => 'details',
+      '#title'  => t('promo settings'),
+      '#description' => t('Administrate settings promo elements such as the promo button or the promo banner.'),
+      '#open'   => true,
+    ];
+
+    $form['palm_promo_settings']['palm_header_promo_button_link'] = [
+        '#type'           => 'textfield',
+        '#title'          => t('URL for the promo button link'),
+        '#description'    => t('Please provide the url of the promo link. Leave empty to disable the promo button.'),
+        '#default_value'  => theme_get_setting('palm_header_promo_button_link'),
+    ];
+    $form['palm_promo_settings']['palm_header_promo_button_link_title'] = [
+        '#type'           => 'textfield',
+        '#title'          => t('title for promo button link'),
+        '#description'    => t('Please provide a title for the promo button in the footer.'),
+        '#default_value'  => theme_get_setting('palm_header_promo_button_link_title'),
+    ];
+
+    //settings for the promo banner on the bottom of the page
+    $form['palm_promo_settings']['palm_promo_banner_state'] = [
+      '#type'           => 'select',
+      '#title'          => t('State for promo banner'),
+      '#description'    => t('Please provide a state for the promo banner.'),
+      '#default_value'  => theme_get_setting('palm_promo_banner_state'),
+      '#options'        => [0 => t('inactive'), 1 => t('active')],
+    ];
+    $form['palm_promo_settings']['palm_promo_banner_title'] = [
+      '#type'           => 'textfield',
+      '#title'          => t('title for promo banner'),
+      '#description'    => t('Please provide a title for the promo banner.'),
+      '#default_value'  => theme_get_setting('palm_promo_banner_title'),
+    ];
+    $form['palm_promo_settings']['palm_promo_banner_claim'] = [
+      '#type'           => 'textfield',
+      '#title'          => t('Claim for promo banner'),
+      '#description'    => t('Please provide a claim for the promo banner.'),
+      '#default_value'  => theme_get_setting('palm_promo_banner_claim'),
+    ];
+    $form['palm_promo_settings']['palm_promo_banner_description'] = [
+      '#type'           => 'textfield',
+      '#title'          => t('Description for promo banner'),
+      '#description'    => t('Please provide a description for the promo banner.'),
+      '#default_value'  => theme_get_setting('palm_promo_banner_description'),
+    ];
+    $form['palm_promo_settings']['palm_promo_banner_link'] = [
+      '#type'           => 'textfield',
+      '#title'          => t('Link for promo banner'),
+      '#description'    => t(
+        'Please provide a link for the promo banner. It should be either a full URL or relative starting with a /.'
+      ),
+      '#default_value'  => theme_get_setting('palm_promo_banner_link'),
+    ];
+    $form['palm_promo_settings']['palm_promo_banner_image_url'] = [
+      '#type'           => 'textfield',
+      '#title'          => t('Image url for promo banner'),
+      '#description'    => t(
+        'Please provide a url for the promo banner image. It should be relative starting with a /.'
+      ),
+      '#default_value'  => theme_get_setting('palm_promo_banner_image_url'),
+    ];
+
 }
